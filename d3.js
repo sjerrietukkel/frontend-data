2f6b039de45444aaf08d364c2336affa6d51a7f8
sjerrietukkel <danielvdschuur@gmail.com>
2018-11-22 22:25:53 +0100

    added search

--- d3.js
+++ d3.js
@@ -1,75 +1,77 @@
 // Based on https://bl.ocks.org/gordlea/27370d1eea8464b04538e6d8ced39e89
-d3.csv("duitsesteden.csv").then(function(data) {
-    return data
-})
+// d3.csv("duitsesteden.csv").then(function(data) {
+//     return data
+// })
 
-.then(data => {  
-  d3.json("data/all.json").then(function(bookdata){
-    var duitseSteden = data.map(d => {
-        return d.publication.replace(/[^a-zA-Z ]/g, "")
-    })
-    var publicationYear = bookdata.map(d => {
-        if (d.pubYear == null) { 
-            return ""; 
-        }
-        return d.pubYear
-    })
 
-    var publicationCity = bookdata.map(d => {
-        if (d.publication == null) { 
-            return "";
-        }
-        return d.publication.replace(/[^a-zA-Z ]/g, "")
-    })
+
+// .then(data => {  
+//   d3.json("data/all.json").then(function(bookdata){
+//     var duitseSteden = data.map(d => {
+//         return d.publication.replace(/[^a-zA-Z ]/g, "")
+//     })
+//     var publicationYear = bookdata.map(d => {
+//         if (d.pubYear == null) { 
+//             return ""; 
+//         }
+//         return d.pubYear
+//     })
+
+//     var publicationCity = bookdata.map(d => {
+//         if (d.publication == null) { 
+//             return "";
+//         }
+//         return d.publication.replace(/[^a-zA-Z ]/g, "")
+//     })
     
 
-    var cityArray = []
-    // duitseSteden.forEach(city => {
-    //     if (publicationCity.includes(city)  ) {
-    //         cityArray.push(city)
-    //     }
-    //     else {
-    //         // console.log('nietus')
-    //     }
-    // })
+//     var cityArray = []
+//     // duitseSteden.forEach(city => {
+//     //     if (publicationCity.includes(city)  ) {
+//     //         cityArray.push(city)
+//     //     }
+//     //     else {
+//     //         // console.log('nietus')
+//     //     }
+//     // })
 
-    // console.log(publicationCity)
-    // console.log(publicationYear)
-    // console.log(cityArray)
-    // console.log(bookdata)
+//     // console.log(publicationCity)
+//     // console.log(publicationYear)
+//     // console.log(cityArray)
+//     // console.log(bookdata)
 
-    // duitseSteden.filter(city => {
-    //     // console.log(city)
-    //     if (bookdata.publication.includes(city)) {
-    //         cityArray.push(bookdata)
-    //     }
-    // })
+//     // duitseSteden.filter(city => {
+//     //     // console.log(city)
+//     //     if (bookdata.publication.includes(city)) {
+//     //         cityArray.push(bookdata)
+//     //     }
+//     // })
 
-    // publicationCity.forEach(book => {
-    //     // console.log(book)
-    //     if(publicationCity.includes(duitseSteden)) {
-    //         console.log('check')
-    //     }
-    //     else {
+//     // publicationCity.forEach(book => {
+//     //     // console.log(book)
+//     //     if(publicationCity.includes(duitseSteden)) {
+//     //         console.log('check')
+//     //     }
+//     //     else {
 
-    //     }
-    // })
+//     //     }
+//     // })
 
-    return bookdata
-  })
-  return data
-})
+//     return bookdata
+//   })
+//   return data
+// })
 
 d3.json("data/all.json").then(function(data) {
-    var bookNested = d3
-        .nest()
-        .key(data => data.pubYear)
-        // .key(data => data.publication)
-        .rollup(function(data) {
-            return data.length
+    // var bookNested = d3
+    //     .nest()
+    //     .key(data => data.pubYear)
+    //     // .key(data => data.publication)
+    //     .rollup(function(data) {
+    //         return data.length
             
-        })
-        .entries(data)
+    //     })
+    //     .entries(data)
 
     // bookNested.forEach(city => {
     //     if (publication.includes(city) ) {
@@ -96,147 +98,147 @@ d3.json("data/all.json").then(function(data) {
         }
     })
 
-    function oneOfCities(city) {
-        switch (city) {
-            case 'Amsterdam':
-            case 'Berlin':
-            case 'Kln':
-            case 'Zrich':
-            case 'Frankfurt am Main':
-            case 'Mnchen':
-            case 'Wien':
-                return true
-            default:
-                return false
-        }
-    }
+    let cityList = ['Berlin', 'Kln',  'Frankfurt am Main', 'Mnchen', 'Leipzig']
+    let dataList = []
 
 
-    let sortedByYear = []
-    for (let i = 1900; i <= 2018; i++) {
-        let thisYear = {
-            year: i,
-            values: []
-        }
+    let submit = document.querySelector("#submit").addEventListener('click', addToCityList)
+    // var inputValue = document.getElementById('box').value
+    function addToCityList() {
+        cityList.push(document.getElementById('box').value); 
+        update(createDataList(cityList))
+        console.log(cityList)
+      }
 
-        publicationCity.forEach(book => {
-            let thisCity = book.publication
-
-            function findCity(bookItem) {
-                return bookItem.city === book.publication
+      
+    function createDataList(list){
+        const result = [];
+        list.forEach(city => {
+            thisCityValues = {
+                city: city,
+                value: []
             }
 
-            if (oneOfCities(book.publication) === false) {
-                return
-            }
-
-            if (Number(book.pubYear) === i) {
-                let arrayItem = thisYear.values.find(findCity)
-                let index = thisYear.values.findIndex(findCity)
-                if (index === -1) {
-                    thisYear.values.push({city: book.publication, value: 1})
-                } else if (index >= 0) {
-                    thisYear.values.splice(index, 1)
-                    thisYear.values.push({city: book.publication, value: arrayItem.value + 1})
+            for (let i = 1920; i <= 1970; i++) {
+                let thisYear = {
+                    year: i,
+                    value: 0
                 }
-            } else {
-                let index = thisYear.values.findIndex(findCity)
 
-                if (index >= 0) {
-                    return
-                }
-                thisYear.values.push({city: book.publication, value: 0})
+                publicationCity.forEach(book => {
+                    if(book.publication != city) {
+                        return
+                    }
+
+                    if (Number(book.pubYear) === i) {
+                        thisYear.value++
+                    }
+                })
+
+                thisCityValues.value.push(thisYear)
             }
+
+            result.push(thisCityValues)
         })
+        return result
 
-        thisYear.values.sort(function(a, b) {
-            var textA = a.city.toUpperCase();
-            var textB = b.city.toUpperCase();
-            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        });
-
-        sortedByYear.push(thisYear)
     }
-    return sortedByYear
-})
-
-.then(d => {
-    console.log(d)
-
-    let body = d3.select('body')
-    let svg = body
-    .append('svg')
-    .attr('width', 1000)
-    .attr('height', 1000)
-
-    let yearCount = d.map(d => {
-        return Number(d.key)
-    })  
+    
 
 
-    let bookCount = d.map(d => {
-        return Number(d.value)
-    }) 
+    
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
+    var parseTime = d3.timeParse("%Y");
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
 
+    var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .classed("holder", true)
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-    let Xscale = d3.scaleLinear()
-        .range ([0, 700])
-        .domain([1800, 2018])
-        // .domain([d3.min(), d3.max(yearCount)])
+    x.domain([parseTime(1920), parseTime(1970)]);
+    y.domain([0, 50]);
 
+    // Add the X axis
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-    let Yscale = d3.scaleLinear()
-        .range([0, 500])
-        .domain([600, 0])    
-        
-        // .domain([
-        //     d3.max(d, d=> {
-        //         return formatNumbers(d)
-        //     })
+    // Add the Y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+    
             
-        // ])    
+    update(createDataList(cityList))
+    function update(list){
 
-    let x_Axis = d3
-        .axisBottom()
-        .scale(Xscale)
-        .tickFormat(d3.format("d"));
+        var valueline = d3.line()
+            .x(function(d) { return x(parseTime(d.year)); })
+            .y(function(d) { return y(d.value); });
 
-    let y_Axis = d3.axisLeft().scale(Yscale);
-   
-    let xAxisGroup = svg
-        .append("g")
-        .attr("transform", "translate(0, " + 500 + ")")
-        .call(x_Axis);
-        
-    let yAxisGroup = svg
-        .append("g")
-        .call(y_Axis);        
+        list.forEach(city => {
+            svg
+                .data([city.value])
+                .append("path")
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-linecap", "round")
+                .attr("class", `line ${city.city}`)
+                .attr("d", valueline)
 
-
-
-    function formatNumbers(d) {
-        let format = "1985"
-        let formatIndex = d.values.findIndex(x => x.key === format)
-        if (formatIndex!== -1) {
-            return d.values[formatIndex].value.formatCount
-        } else {
-            return 0
-        }
+        })
     }
 
-    svg.selectAll("line")
-    .data(d)
-    .enter()
-    .append("circle")
-    .style('fill', 'orange')
-    .style('opacity', '.3')
-    .attr("r", 5)
-    .attr("cx", d => {
-        return Xscale(Number(d.key))
-    })
-    .attr("cy", d => {
-        return Yscale(Number(d.value))
-    })
+//     let cities = []
+//     for (let i = 1950; i <= 2018; i++) {
+//         let thisCity = {
+//             city: i,
+//             values: [
+//                 {year: i, value: 2}
+//             ]
+//         }
 
+//         publicationCity.forEach(book => {
+//             let thisCity = book.publication
+//             function findCity(bookItem) {
+//                 return bookItem.city === book.publication
+//             }
+//             if (oneOfCities(book.publication) === false) {
+//                 return
+//             }
+//             if (Number(book.pubYear) === i) {
+//                 let arrayItem = thisYear.values.find(findCity)
+//                 let index = thisYear.values.findIndex(findCity)
+//                 if (index === -1) {
+//                     thisYear.values.push({city: book.publication, value: 1})
+//                 } else if (index >= 0) {
+//                     thisYear.values.splice(index, 1)
+//                     thisYear.values.push({city: book.publication, value: arrayItem.value + 1})
+//                 }
+//             } else {
+//                 let index = thisYear.values.findIndex(findCity)
+
+//                 if (index >= 0) {
+//                     return
+//                 }
+//                 thisYear.values.push({city: book.publication, value: 0})
+//             }
+//         })
+
+//         thisYear.values.sort(function(a, b) {
+//             var textA = a.city.toUpperCase();
+//             var textB = b.city.toUpperCase();
+//             return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+//         });
+
+//         sortedByYear.push(thisYear)
+//     }
+//     return sortedByYear
 })
+
